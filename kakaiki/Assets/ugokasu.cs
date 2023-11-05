b156a186202480befe770402c73f34ef5dfcbe34
ワンダー横浜EXコース <wdyokohama.ex@gmail.com>
2023-11-05 11:18:42 +0900

    11月5日

---
+++ kakaiki/Assets/ugokasu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ugokasu : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() 
+
+        {
+            rb.AddForce(0,0,-5);
+        }
+    
+}
